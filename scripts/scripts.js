6342adbdbadba40f4866933f35fb0e11c86fa804
Mike Nealon <chisao101@gmail.com>
2020-10-09 21:42:07 -0400

    Got some basics done
    Still working on the design and stuff.

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+/*  */
+
+function openSideMenu(){
+    document.querySelector('.open-side-menu').style.width = '250px';
+    document.querySelector('').style.marginLeft = '250px';
+}
+
+function closeSideMenu(){
+    
+}
